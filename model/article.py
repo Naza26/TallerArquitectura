60f58e7bbdaccdfe8b479c8f9d95429f1314e508
Nazarena Rueda <naza120@gmail.com>
2023-10-03 12:21:08 -0300

    refactor to not break encapsulation

--- model/article.py
+++ model/article.py
@@ -9,8 +9,14 @@ class Article:
     def has_text(self, text):
         return self._text == text
 
-    def title(self):
-        return self._title
+    def contains_too_short_title(self):
+        return len(self._title) < 2
 
-    def text(self):
-        return self._text
+    def contains_too_long_title(self):
+        return len(self._title) > 50
+
+    def contains_too_short_text(self):
+        return len(self._text) < 1800
+
+    def contains_too_long_text(self):
+        return len(self._text) > 5200
--- model/magazine.py
+++ model/magazine.py
@@ -7,12 +7,12 @@ class Magazine:
         return self._articles
 
     def publish(self, article):
-        if len(article.title()) < 2:
+        if article.contains_too_short_title():
             raise Exception("Title cannot have less than two characters")
-        if len(article.title()) > 50:
+        if article.contains_too_long_title():
             raise Exception("Title cannot have more than fifty characters")
-        if len(article.text()) < 1800:
+        if article.contains_too_short_text():
             raise Exception("Text cannot have less than 1800 characters")
-        if len(article.text()) > 5200:
+        if article.contains_too_long_text():
             raise Exception("Text cannot have more than 5200 characters")
         self._articles.append(article)
