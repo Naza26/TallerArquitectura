e1a9bde1492f58dcf5b9ac5a5521457b2d6a5af9
Nazarena Rueda <naza120@gmail.com>
2023-10-09 12:05:05 -0300

    wip

--- tests/test.py
+++ tests/test.py
@@ -27,13 +27,13 @@ class MagazineTests(unittest.TestCase):
 
     def test_03_when_published_an_article_has_a_title(self):
         magazine = self.catalog.empty_magazine()
-        standard_article_text = self.catalog.standard_article_text()
-        article = self.catalog.create_article(text=standard_article_text)
+        standard_article_title = self.catalog.standard_article_title()
+        article = self.catalog.create_article(title=standard_article_title)
 
         magazine.publish(article)
 
         an_article = self.catalog.get_an_article_from(magazine.list_of_articles())
-        self._assert_proper_title_is_contained_in(an_article)
+        self._assert_proper_article_is_contained_in_magazine(an_article, an_article.has_title, standard_article_title)
 
     def test_04_when_published_an_article_has_a_text(self):
         magazine = self.catalog.empty_magazine()
@@ -43,7 +43,7 @@ class MagazineTests(unittest.TestCase):
         magazine.publish(article)
 
         an_article = self.catalog.get_an_article_from(magazine.list_of_articles())
-        self._assert_proper_title_is_contained_in(an_article)
+        self._assert_proper_article_is_contained_in_magazine(an_article, an_article.has_text, standard_article_text)
 
     def test_05_title_of_article_cannot_be_too_short(self):
         magazine = self.catalog.empty_magazine()
@@ -85,8 +85,8 @@ class MagazineTests(unittest.TestCase):
             magazine.publish(article)
         return result
 
-    def _assert_proper_title_is_contained_in(self, an_article):
-        self.assertTrue(an_article.has_title(self.catalog.standard_article_title()))
+    def _assert_proper_article_is_contained_in_magazine(self, an_article, has_valid_content, content):
+        self.assertTrue(has_valid_content(content))
         self.assertFalse(an_article.has_title("xxx"))
 
 
