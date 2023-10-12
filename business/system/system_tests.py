607d72a0c174c9c251cf5d346ecc8c78d0582914
Nazarena Rueda <naza120@gmail.com>
2023-10-12 15:47:42 -0300

    wip

--- business/system/system_tests.py
+++ business/system/system_tests.py
@@ -20,13 +20,13 @@ class MagazineSystemTests(unittest.TestCase):
 
     def test02_system_can_publish_an_article(self):
         system = MagazineSystem(self.catalog.empty_magazine())
-        article_to_serialize = {"title": "x" * Article.MINIMUM_TITLE_LENGTH, "text": "x" * Article.MINIMUM_TEXT_LENGTH}
-        article_to_publish = system.create_serialized_article(article_to_serialize)
 
-        system.publish(article_to_publish)
+        for article_to_serialize in system.unserialized_sample_articles():
+            article_to_publish = system.create_serialized_article(article_to_serialize)
+            system.publish(article_to_publish)
 
         articles = system.list_of_articles()
-        self.assertEqual(len(articles), 1)
+        self.assertEqual(len(articles), 3)
 
     def test03_system_can_publish_articles_with_valid_titles(self):
         system = MagazineSystem(self.catalog.empty_magazine())
